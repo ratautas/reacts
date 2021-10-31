f79549798e94bab5ea28836a49d67e5c8b98bdb1
algirdas-zyro <algirdas@zyro.com>
2021-10-31 09:24:43 +0200

    Update App.tsx template with fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import HelloWorld from "@/components/HelloWorld";
 
 const App = (): JSX.Element => {
-  return <HelloWorld />;
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
 };
 
 export default App;
