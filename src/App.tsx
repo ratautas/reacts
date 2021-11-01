354a92736a1fbc418641a9a30798841b07ee0bc9
ratautas <algirdas.tamasauskas@gmail.com>
2021-11-01 09:38:00 +0200

    Add initial reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import "@/assets/style.scss";
 
+import { useReducer } from "react";
+
 import HelloWorld from "@/components/HelloWorld";
 
+export enum ActionTypes {
+  DO_STUFF = "DO_STUFF",
+}
+
+export type Action = {
+  type: string;
+  payload?: unknown;
+};
+
+export type State = {};
+
+const INITIAL_STATE = {};
+const reducer = (state: State, { type, payload }: Action) => {
+  return state;
+};
+
 const App = (): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
   return (
     <>
       <HelloWorld />
